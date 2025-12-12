ca275c023a07217b3125e59fce9f9258ee90eb3f
João pedro <joaopedroservico1@gmail.com>
2025-12-12 11:56:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ Mensagem: ${mensagem}
     }
 });
 
-app.listen(3000, () => {
-    console.log("API rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("API rodando na porta " + PORT);
 });
